62c779e33a7d8b709040bbc7899d47358eb24e0d
Fikri Rasyid <fikrirasyid@gmail.com>
2023-08-12 23:35:56 +0700

    Initialize repo. The plugin is fully working at this point. build output result is committed so  the extension is fully working without the need to run the build process once the repo is cloned

---
+++ server/index.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace D5TUTSimpleQuickModule;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Direct access forbidden.' );
+}
+
+require_once ABSPATH . 'wp-content/themes/Divi/includes/builder-5/server/Framework/DependencyManagement/Interfaces/DependencyInterface.php';
+
+use ET\Builder\Framework\DependencyManagement\Interfaces\DependencyInterface;
+use ET\Builder\Framework\Utility\HTMLUtility;
+use ET\Builder\FrontEnd\BlockParser\BlockParserStore;
+use ET\Builder\FrontEnd\Module\Style;
+use ET\Builder\Packages\Module\Module;
+use ET\Builder\Packages\Module\Options\Element\ElementClassnames;
+use ET\Builder\Packages\Module\Options\Element\ElementComponents;
+use ET\Builder\Packages\Module\Options\Element\ElementScriptData;
+use ET\Builder\Packages\Module\Options\Element\ElementStyle;
+use ET\Builder\Packages\ModuleLibrary\ModuleRegistration;
+
+/**
+ * Class that handle "Simple Quick Module" module output in frontend.
+ */
+class D5TutorialSimpleQuickModule implements DependencyInterface {
+	/**
+	 * Register module.
+	 * `DependencyInterface` interface ensures class method name `load()` is executed for initialization.
+	 */
+	public function load() {
+		// Path to module metadata that is shared between Frontend and Visual Builder.
+		$module_json_folder_path = dirname( __DIR__, 1 ) . '/visual-builder/src';
+
+		// Register module.
+		add_action(
+			'init',
+			function() use ( $module_json_folder_path ) {
+				ModuleRegistration::register_module(
+					$module_json_folder_path,
+					[
+						'render_callback' => [ D5TutorialSimpleQuickModule::class, 'render_callback' ],
+					]
+				);
+			}
+		);
+	}
+
+	/**
+	 * Render module style.
+	 */
+	public static function module_styles( $args ) {
+		$attrs = $args['attrs'] ?? [];
+
+		Style::add(
+			[
+				'id'            => $args['id'],
+				'name'          => $args['name'],
+				'orderIndex'    => $args['orderIndex'],
+				'storeInstance' => $args['storeInstance'],
+				'styles'        => [
+					// Module.
+					ElementStyle::style(
+						[
+							'selector'   => $args['orderClass'],
+							'attrs'      => $attrs['module']['decoration'] ?? [],
+							'disabledOn' => [
+								'disabledModuleVisibility' => $args['settings']['disabledModuleVisibility'] ?? null,
+							],
+						]
+					),
+					// Title.
+					ElementStyle::style(
+						[
+							'attrs'    => $attrs['title']['decoration'] ?? [],
+							'selector' => "{$args['orderClass']} .d5_tut_simple_quick_module_title",
+						]
+					),
+					// Content.
+					ElementStyle::style(
+						[
+							'selector' => "{$args['orderClass']} .d5_tut_simple_quick_module_content",
+							'attrs'    => $attrs['content']['decoration'] ?? [],
+						]
+					),
+				],
+			]
+		);
+	}
+
+	/**
+	 * Render module script data.
+	 */
+	public static function module_script_data( $args ) {
+		$id             = $args['id'] ?? '';
+		$selector       = $args['selector'] ?? '';
+		$attrs          = $args['attrs'] ?? [];
+		$store_instance = $args['storeInstance'] ?? null;
+
+		// Element Script Data Options.
+		ElementScriptData::set(
+			[
+				'id'            => $id,
+				'selector'      => $selector,
+				'attrs'         => $attrs['module']['decoration'] ?? [],
+				'storeInstance' => $store_instance,
+			]
+		);
+	}
+
+	/**
+	 * Render module classnames.
+	 */
+	public static function module_classnames( $args ) {
+		$classnames_instance = $args['classnamesInstance'];
+		$attrs               = $args['attrs'];
+
+		// Module.
+		$classnames_instance->add(
+			ElementClassnames::classnames(
+				[
+					'attrs' => $attrs['module']['decoration'] ?? [],
+				]
+			)
+		);
+	}
+
+	/**
+	 * Render module HTML output.
+	 */
+	public static function render_callback( $attrs, $content, $block, $elements ) {
+		// Title.
+		$header = $elements->render(
+			[
+				'attrName' => 'title',
+			]
+		);
+
+		// Content.
+		$content = $elements->render(
+			[
+				'attrName' => 'content',
+			]
+		);
+
+		$description = HTMLUtility::render(
+			[
+				'tag'               => 'div',
+				'attributes'        => [
+					'class' => 'et_pb_module_inner',
+				],
+				'childrenSanitizer' => 'et_core_esc_previously',
+				'children'          => $header . $content,
+			]
+		);
+
+		$parent = BlockParserStore::get_parent( $block->parsed_block['id'], $block->parsed_block['storeInstance'] );
+
+		return Module::render(
+			[
+				// FE only.
+				'orderIndex'          => $block->parsed_block['orderIndex'],
+				'storeInstance'       => $block->parsed_block['storeInstance'],
+
+				// VB equivalent.
+				'attrs'               => $attrs,
+				'id'                  => $block->parsed_block['id'],
+				'moduleClassName'     => 'd5_tut_simple_quick_module',
+				'name'                => $block->block_type->name,
+				'classnamesFunction'  => [ D5TutorialSimpleQuickModule::class, 'module_classnames' ],
+				'moduleCategory'      => $block->block_type->category,
+				'stylesComponent'     => [ D5TutorialSimpleQuickModule::class, 'module_styles' ],
+				'scriptDataComponent' => [ D5TutorialSimpleQuickModule::class, 'module_script_data' ],
+				'parentAttrs'         => $parent->attrs ?? [],
+				'parentId'            => $parent->id ?? '',
+				'parentName'          => $parent->blockName ?? '',
+				'children'            => ElementComponents::component(
+					[
+						'attrs'         => $attrs['module']['decoration'] ?? [],
+						'id'            => $block->parsed_block['id'],
+
+						// FE only.
+						'orderIndex'    => $block->parsed_block['orderIndex'],
+						'storeInstance' => $block->parsed_block['storeInstance'],
+					]
+				) . $description,
+			]
+		);
+	}
+
+}
+
+// Register module.
+add_action(
+	'et_module_library_modules_dependency_tree',
+	function( $dependency_tree ) {
+			$dependency_tree->add_dependency( new D5TutorialSimpleQuickModule() );
+	}
+);
---
+++ visual-builder/src/index.jsx
@@ -0,0 +1,264 @@
+// External library dependencies.
+import React from 'react';
+
+// WordPress package dependencies.
+const {
+  addAction,
+} = window?.wp?.hooks;
+
+// Divi package dependencies.
+const {
+  RichTextContainer,
+  TextContainer,
+} = window?.divi?.fieldLibrary;
+const {
+  GroupContainer
+} = window?.divi?.modal;
+const {
+  // Renderer - HTML
+  ElementComponents,
+  ModuleContainer,
+
+  // Renderer - Styles
+  ElementStyle,
+  StyleContainer,
+
+  // Renderer - Classnames
+  elementClassnames,
+
+  // Renderer - ScriptData
+  useElementScriptData,
+
+  // Settings - Content
+  AdminLabelGroup,
+  BackgroundGroup,
+  FieldContainer,
+
+  // Settings - Content
+  AnimationGroup,
+  BorderGroup,
+  BoxShadowGroup,
+  FiltersGroup,
+  FontGroup,
+  FontBodyGroup,
+  SizingGroup,
+  SpacingGroup,
+  TransformGroup,
+
+  // Settings - Advanced
+  PositionSettingsGroup,
+  ScrollSettingsGroup,
+  TransitionGroup,
+  VisibilitySettingsGroup,
+} = window?.divi?.module;
+const {
+  registerModule
+} = window?.divi?.moduleLibrary;
+
+// Module metadata that is used in both Frontend and Visual Builder.
+import metadata from './module.json';
+
+/**
+ * React function component for rendering module style.
+ */
+const ModuleStyles = ({
+  attrs,
+  settings,
+  orderClass,
+  mode,
+  state,
+  noStyleTag
+}) => (
+  <StyleContainer mode={mode} state={state} noStyleTag={noStyleTag}>
+    {/* Element: Module */}
+    <ElementStyle
+      attrs={attrs?.module?.decoration ?? {}}
+      selector={orderClass}
+      disabledOn={{
+        disabledModuleVisibility: settings?.disabledModuleVisibility
+      }}
+    />
+
+    {/* Element: Title */}
+    <ElementStyle
+      attrs={attrs?.title?.decoration ?? {}}
+      selector={`${orderClass} .d5_tut_simple_quick_module_title`}
+    />
+
+    {/* Element: Content */}
+    <ElementStyle
+      selector={`${orderClass} .d5_tut_simple_quick_module_content`}
+      attrs={attrs?.content?.decoration ?? {}}
+    />
+  </StyleContainer>
+);
+
+/**
+ * React function component for registering module script data.
+ */
+const ModuleScriptData = ({
+  attrs,
+  selector,
+  id,
+}) => {
+  useElementScriptData({
+    attrs: attrs?.module?.decoration,
+    selector,
+    id,
+  });
+  return null;
+}
+
+/**
+ * Function for registering module classnames.
+ */
+const moduleClassnames = ({
+  classnamesInstance,
+  attrs,
+}) => {
+  // Add element classnames.
+  classnamesInstance.add(
+    elementClassnames({
+      attrs: attrs?.module?.decoration ?? {},
+    }),
+  );
+}
+
+/**
+ * Simple Quick Module.
+ */
+const simpleQuickModule = {
+  // Metadata that is used on Visual Builder and Frontend
+  metadata,
+
+  // Layout renderer components.
+  renderers: {
+    // React Function Component for rendering module's output on layout area.
+    edit: ({
+      attrs,
+      id,
+      name,
+      elements,
+    }) => (
+      <ModuleContainer
+        attrs={attrs}
+        id={id}
+        moduleClassName="d5_tut_simple_quick_module"
+        name={name}
+        scriptDataComponent={ModuleScriptData}
+        stylesComponent={ModuleStyles}
+        classnamesFunction={moduleClassnames}
+      >
+        <ElementComponents
+          attrs={attrs?.module?.decoration}
+          id={id}
+        />
+        <div className="et_pb_module_inner">
+          {elements.render({
+            attrName: 'title',
+          })}
+          {elements.render({
+            attrName: 'content',
+          })}
+        </div>
+      </ModuleContainer>
+    ),
+  },
+
+  // Settings component.
+  settings: {
+    // React function component that renders module settings' content panel.
+    content: ({ defaultSettingsAttrs }) => (
+      <React.Fragment>
+        <GroupContainer
+          id="mainContent"
+          title="Text"
+        >
+          <FieldContainer
+            attrName="title.innerContent"
+            label="Title"
+            description="Title"
+          >
+            <TextContainer />
+          </FieldContainer>
+          <FieldContainer
+            attrName="content.innerContent"
+            label="Content"
+          >
+            <RichTextContainer />
+          </FieldContainer>
+        </GroupContainer>
+        <BackgroundGroup />
+        <AdminLabelGroup
+          defaultGroupAttr={defaultSettingsAttrs?.adminLabel}
+        />
+      </React.Fragment>
+    ),
+
+    // React function component that renders module settings' design panel.
+    design: () => (
+      <React.Fragment>
+        <FontGroup
+          attrName="title.decoration.font"
+          groupLabel="Title Font"
+        />
+        <FontBodyGroup
+          attrName="content.decoration.bodyFont"
+          groupLabel="Content Font"
+        />
+        <SizingGroup />
+        <SpacingGroup />
+        <BorderGroup />
+        <BoxShadowGroup />
+        <FiltersGroup />
+        <TransformGroup />
+        <AnimationGroup />
+      </React.Fragment>
+    ),
+
+    // React function component that renders module settings' advanced panel.
+    advanced: () => (
+      <React.Fragment>
+        <VisibilitySettingsGroup />
+        <TransitionGroup />
+        <PositionSettingsGroup />
+        <ScrollSettingsGroup />
+      </React.Fragment>
+    ),
+  },
+
+  // Attribute that is automatically added into the module when the module is inserted
+  // into the layout so that the newly inserted module has some placeholder content
+  placeholderContent: {
+    module: {
+      decoration: {
+        background: {
+          desktop: {
+            value: {
+              color: '#DFDFDF',
+            }
+          }
+        },
+      }
+    },
+    title: {
+      innerContent: {
+        desktop: {
+          value: 'Module Title'
+        }
+      }
+    },
+    content: {
+      innerContent: {
+        desktop: {
+          value: 'Module Content'
+        }
+      }
+    }
+  },
+};
+
+// Register module.
+addAction('moduleLibrary.registerModuleLibraryStore.after', 'd5Tut.simpleQuickModule', () => {
+  registerModule(simpleQuickModule.metadata, simpleQuickModule);
+});
